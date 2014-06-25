b201fcd20049799228f1e7051d30c9bd737cb3ff
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-25 12:19:02 +1200

    Added shop#load_search_page and #search_url + tests

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,12 @@ class Shop
     results_page = load_search_page query
     parse_search_page results_page
   end
+
+  def load_search_page(query)
+    PageLoader.load_page search_url query
+  end
+
+  def search_url(query)
+    "http://example.com/#{query}"
+  end
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -6,8 +6,8 @@ describe Shop, :type => :model do
   let (:shop) { Shop.new }
 
   before do
-    allow(shop).to receive(:load_search_page).and_return(page)
     allow(shop).to receive(:parse_search_page)
+    allow(PageLoader).to receive(:load_page).and_return(page)
   end
 
   describe "searching" do
@@ -24,5 +24,16 @@ describe Shop, :type => :model do
       expect(shop).to receive(:parse_search_page).with(page)
       shop.search(query)
     end
+  end
+
+  describe "loading the search page" do
+    it "asks PageLoader for the page" do
+      expect(PageLoader).to receive(:load_page).with "http://example.com/test"
+      shop.load_search_page(query)
+    end
+  end
+
+  it "can give a search url for a query" do
+    expect(shop.search_url query).to eq "http://example.com/test"
   end
 end
