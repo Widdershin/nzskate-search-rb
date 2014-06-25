a1c7368043ac3d7cc0e3326baf423157f6be96b7
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-26 11:13:15 +1200

    Added functioning HyperRide plugin + tests

---
+++ app/models/shop_plugins/hyper_ride.rb
@@ -0,0 +1,39 @@
+class HyperRide < Shop
+  def base_url
+    "http://www.hyperride.co.nz"
+  end
+
+  def search_url query
+    "#{base_url}/product/#{sanitize query}/search#/?size=0"
+  end
+
+  def separate_results results_page
+    results_page.css '.product_item'
+  end
+
+  def parse_result_name result_html
+    result_html.at_css('.product_desc').text.strip.to_s
+  end
+
+  def parse_result_url result_html
+    base_url + result_html.at_css('.product_desc')['href']
+  end
+
+  def parse_result_price result_html
+    extract_price result_html.at_css('.rrp').text
+  end
+
+  def parse_result_img result_html
+    result_html.at_css('img')['src']
+  end
+
+private
+
+  def sanitize query
+    query.gsub ' ', '%20'
+  end
+
+  def extract_price rrp
+    /[\d\.]+/.match(rrp).to_s.to_f
+  end
+end
---
+++ spec/models/shop_plugins/hyper_ride_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe HyperRide do
+  let (:shop) { HyperRide.new }
+
+  describe '#search_url' do
+    it 'gives a search url for a query' do
+      expect(shop.search_url 'test').to eq 'http://www.hyperride.co.nz/product/test/search#/?size=0'
+    end
+
+    it 'handles spaces in the query' do
+      expect(shop.search_url 'test post').to eq 'http://www.hyperride.co.nz/product/test%20post/search#/?size=0'
+    end
+  end
+
+  it 'can separate a page into results', :vcr do
+    page = shop.load_search_page('hawgs')
+
+    expect(shop.separate_results page).not_to be_empty
+  end
+
+  describe 'parsing html' do
+    let (:test_html) { load_html_snippet 'hyper-fatty-hawgs' }
+
+    it 'can parse the listing name' do
+      expect(shop.parse_result_name test_html).to eq 'LANDYACHTZ FATTY HAWGS YELLOW 63MM 78A'
+    end
+
+    it 'can parse the listing url' do
+      expect(shop.parse_result_url test_html).to eq 'http://www.hyperride.co.nz/product/longboard-wheels/402/fatty-hawgs-yellow-63mm-78a/1118860/detail'
+    end
+
+    it 'can parse the listing price' do
+      expect(shop.parse_result_price test_html).to eq 79.95
+    end
+
+    it 'can parse the listing img' do
+      expect(shop.parse_result_img test_html).to eq '//www.hyperride.co.nz/content/images/products/10/landyachtz-fatty-hawgs-yellow-63mm-78a-001_S.jpg'
+    end
+  end
+end
