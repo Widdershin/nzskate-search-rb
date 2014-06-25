370baced80598ce7051d5cca7ba61636f99c1a00
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-25 15:11:59 +1200

    Added UltimateBoards#search_url + tests

--- app/models/shop_plugins/ultimate_boards.rb
+++ app/models/shop_plugins/ultimate_boards.rb
@@ -1,2 +1,11 @@
 class UltimateBoards < Shop
+  def search_url(query)
+    "http://www.ultimateboards.co.nz/search/products/#{sanitize_query query}"
+  end
+
+  private
+
+  def sanitize_query(query)
+    query.gsub(' ', '-')
+  end
 end
--- spec/models/shop_plugins/ultimate_boards_spec.rb
+++ spec/models/shop_plugins/ultimate_boards_spec.rb
@@ -3,4 +3,13 @@ require 'rails_helper'
 describe UltimateBoards do
   let (:shop) { UltimateBoards.new }
 
+  describe '#search_url' do
+    it 'gives a search url for a query' do
+      expect(shop.search_url 'test').to eq 'http://www.ultimateboards.co.nz/search/products/test'
+    end
+
+    it 'handles spaces in in the query' do
+      expect(shop.search_url 'test post').to eq 'http://www.ultimateboards.co.nz/search/products/test-post'
+    end
+  end
 end
