179c71aceb2c9a09a86992c3610ffed0bfd56d96
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-26 17:00:49 +1200

    Rejiggered user_can_search feature to test presence of shop names. redid cassettes

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -1,26 +1,31 @@
 require 'rails_helper'
 
 describe "As a user" do
-  describe 'when I visit the homepage' do
+  describe 'when I visit the homepage and search for Paris', :vcr, js: true do
     before do
       visit '/'
-    end
 
-    it 'I can search for items and see results', :vcr, js: true do
-      fill_in "query", with: 'Sector9'
-      click_button "Search"
-      expect(page).to have_content "Race Formula"
-    end
-
-    it 'I can click item titles to go the listing page', :vcr, js:true do
       fill_in "query", with: 'Paris'
-
       click_button "Search"
+    end
 
+    it 'I can see results', js: true do
+      expect(page).to have_content "Paris V2 43* Baseplate"
+    end
+
+    it 'I can click item titles to go the listing page', js:true do
       product_link = find_link 'Paris V2 180mm 50* Black'
 
       expect(product_link).to be_present
       expect(product_link[:href]).to eq 'http://www.ultimateboards.co.nz/paris-v2-180mm-50-black'
+    end
+
+    it 'I can see results from Ultimate Boards' do
+      expect(page).to have_content 'Ultimate Boards'
+    end
+
+    it 'I can see results from Hyper Ride' do
+      expect(page).to have_content 'Hyper Ride'
     end
 
   end
