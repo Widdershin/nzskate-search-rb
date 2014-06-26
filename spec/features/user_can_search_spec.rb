ce5c99a9a38cde2957bdda53bc0215b6ad0fd0ab
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-26 16:15:05 +1200

    Can now click result titles to go to the store page for that result

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -12,5 +12,14 @@ describe "As a user" do
       expect(page).to have_content "Race Formula"
     end
 
+    it 'I can click item titles to go the listing page', :vcr, js:true do
+      fill_in "query", with: 'Paris'
+      click_button "Search"
+
+      click_link 'Paris V2 180mm 50* Black'
+
+      expect(current_url).to eq 'http://www.ultimateboards.co.nz/categories/longboard-trucks/paris/paris-v2-180mm-50-black'
+    end
+
   end
 end
